37f289c57ebc878e97e14e6ea3c3f3922b6e3b63
Bjarte Stien Karlsen <bjarte@bjartek.org>
2012-04-06 00:17:08 +0200

    added salat dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -7,9 +7,14 @@ object ApplicationBuild extends Build {
     val appName         = "computer-database-mongo"
     val appVersion      = "1.0-SNAPSHOT"
 
+    val novusRels = "repo.novus rels" at "http://repo.novus.com/releases/"
+
+
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+      "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT"
     )
+
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
